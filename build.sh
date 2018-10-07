9358358f4bc4140659c278dfbdee80007fc8ce04
Dan <d.c.budris@gmail.com>
2018-10-07 19:00:49 -0400

    updated build

--- build.sh
+++ build.sh
@@ -1,8 +1,4 @@
 #!/env/bin bash
 virtualenv FEC_data_notebook
 
-source ./FEC_data_notebook/bin/activate
-
-pip install -r ./requirements.txt
-
-cd ./src && jupyter notebook
+source ./FEC_data_notebook/bin/activate && pip install -r ./requirements.txt && cd ./src && jupyter notebook
