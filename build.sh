e7641f0ce5cfa9ad9c5a69d2a4b54915413de4c3
Dan <d.c.budris@gmail.com>
2018-10-07 19:14:10 -0400

    build updates

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/env/bin bash
+# Create the basic virtualenv
 virtualenv FEC_data_notebook
 
+mkdir ./data
+mkdir ./data/2016
+
+wget -O ./data/2016/independent_expenditure_2016.csv https://cg-519a459a-0ea3-42c2-b7bc-fa1143481f74.s3-us-gov-west-1.amazonaws.com/bulk-downloads/2016/independent_expenditure_2016.csv
+
+# Activate the virtual env, install dependencies, and start the notebook
 source ./FEC_data_notebook/bin/activate && pip install -r ./requirements.txt && cd ./src && jupyter notebook
